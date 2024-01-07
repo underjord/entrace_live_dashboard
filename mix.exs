7113eaccc80dbda1fc41558e2701329979802985
Lars Wikman <lars@underjord.io>
2024-01-07 22:05:13 +0100

    Fix deps

--- mix.exs
+++ mix.exs
@@ -33,18 +33,10 @@ defmodule EntraceLiveDashboard.MixProject do
   defp deps do
     [
       {:phoenix, "~> 1.7.7"},
-      {:phoenix_html, "~> 3.3"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.19.0"},
-      {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.0"},
-      {:esbuild, "~> 0.7", runtime: Mix.env() == :dev},
-      {:telemetry_metrics, "~> 0.6"},
-      {:telemetry_poller, "~> 1.0"},
-      {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"},
-      {:entrace, path: "../entrace"}
+      {:entrace, github: "lawik/entrace"}
     ]
   end
 
