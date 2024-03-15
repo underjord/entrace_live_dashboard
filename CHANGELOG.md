fc5173bb339c30701d6ddeda89504ba62321b5ac
Lars Wikman <lars@underjord.io>
2024-03-15 10:15:13 +0100

    Update for v0.1.1

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# Changelog for Entrace Live Dashboard
+
+## v0.1.1
+
+- Relaxed dependencies significantly (thanks to Valian)
+
+## v0.1.0
+
+- Initial release
+- Kinda rough UX & UI
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EntraceLiveDashboard.MixProject do
   def project do
     [
       app: :entrace_live_dashboard,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
